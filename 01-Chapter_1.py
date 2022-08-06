e97ede6af344f072af62b30027bd462c2b9a979b
JA2005 <76474307+JA2005@users.noreply.github.com>
2022-08-06 17:56:26 +1000

    Update 01-Chapter_1.py

--- 01-Chapter_1.py
+++ 01-Chapter_1.py
@@ -1,10 +1,10 @@
 # This section explains how to read and display image, it is important to make a folder with all the pictures or videos
 # I have all my pictures in resources folder, which is a subfolder in the folder OpenCV
 # The first step will be to import library which is cv2
+
 import cv2 as cv
 
-# Now I am going to read an image
-# This line of code will only read an image 
+# Now I am going to read an image, below line of code will only read an image 
 img = cv.imread("resources/image_grey.png") 
 
 # This line of code will show the image
